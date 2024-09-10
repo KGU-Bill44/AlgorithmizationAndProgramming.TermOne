20e468fbaf91146a5a97f0ac2d39c865d0ee1885
user <user@E-AUD325-13>
2024-09-10 12:14:06 +0300

    Добавьте файлы проекта.

---
+++ AlgorithmizationAndProgramming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgorithmizationAndProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int radius = 0;
+
+            Console.Write("Введите радиус окружности: ");
+            string anyString = Console.ReadLine();
+
+            if (int.TryParse(anyString, out radius))
+            {
+                Console.WriteLine("Площадь = " + MathUtil.getAreaFromRadius(radius));
+                Console.WriteLine("Длинна = " + MathUtil.getLengthAreaFromRadius(radius));
+            }
+            else
+            {
+                Console.WriteLine("Введено не число");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AlgorithmizationAndProgramming/Util/MathUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmizationAndProgramming
+{
+    public static class MathUtil
+    {
+
+        public static double getLengthAreaFromRadius(int radius)
+        {
+            return radius * 2 * Math.PI;
+        }
+
+        public static double getAreaFromRadius(int radius)
+        {
+            return radius * Math.Pow(radius, 2) * Math.PI;
+        }
+    }
+}
