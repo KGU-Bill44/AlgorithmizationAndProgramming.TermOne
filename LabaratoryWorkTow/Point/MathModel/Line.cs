86db5c0a05f568e429b25cb729e0ac778f6795f5
Bill44-NotAlone <denyc9@mail.ru>
2024-09-17 19:12:11 +0300

    LWT FIX 2

---
+++ LabaratoryWorkTow/Point/MathModel/Line.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LabaratoryWorkTow.Point.MathModel
+{
+    public class Line
+    {
+        private Point pointOne;
+        private Point pointTow;
+
+        private double k;
+        private double b;
+
+        public Line(Point pointOne, Point pointTow) {
+            this.pointOne = pointOne;
+            this.pointTow = pointTow;
+
+            createLineFunction(pointOne, pointTow);
+        }
+
+        private void createLineFunction(Point pointOne, Point pointTow)
+        {
+            // y = kx + b
+            double dkopx = pointTow.X - pointOne.X;
+            double dkopy = pointTow.Y - pointOne.Y;
+        }
+
+        public Point PointOne {
+            get { 
+                return pointOne; 
+            }
+        }
+
+        public Point PointTow
+        {
+            get { 
+                return pointTow; 
+            }
+        }
+    }
+}
---
+++ LabaratoryWorkTow/Point/Place/PlaceMath.cs
@@ -0,0 +1,22 @@
+﻿using LabaratoryWorkTow.Point.MathModel;
+
+namespace LabaratoryWorkTow.Point.Plane
+{
+    public class PlaceMath
+    {
+        public Line Line { get; set; }
+
+        public PlaceMath(Line line)
+        {
+            Line = line;
+        }
+
+        public PlaceMath(MathModel.Point pointOne, MathModel.Point pointTow) {
+            Line = new Line(pointOne, pointTow);
+        }
+
+        public bool IsPointOnLine(MathModel.Point point) {
+            
+        }
+    }
+}
