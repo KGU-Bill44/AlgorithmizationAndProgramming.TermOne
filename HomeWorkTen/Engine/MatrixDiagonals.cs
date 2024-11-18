93b17d0c4edee098b0b60aefbf78267377683e81
Bill44-NotAlone <denyc9@mail.ru>
2024-11-18 18:31:08 +0300

    fix HomeWorkTen

--- HomeWorkTen/Engine/MatrixDiagonals.cs
+++ HomeWorkTen/Engine/MatrixDiagonals.cs
@@ -1,15 +1,26 @@
 ﻿namespace HomeWorkTen.Engine;
 
+/// <summary>
+/// Класс, для управления матричными диоганалями.
+/// </summary>
 public static class MatrixDiagonals
 {
+
+    /// <summary>
+    /// Высчитывает среднее арефметическое побочной диаганали квадратной матрицы. 
+    /// </summary>
     public static double AverageSideDiagonal(double[,] matrix)
     {
         int countOfRows = matrix.GetLength(0);
-        int countOfColumns = matrix.GetLength(1);
-        int indexOfColumn = countOfColumns - 1;
+
+        if (countOfRows == 0)
+        {
+            return 0;
+        }
+
         double sum = 0;
         int cuont = 0;
-        
+
         for (int row = countOfRows - 1; row > -1; row--)
         {
             sum = sum + matrix[row, cuont];
@@ -18,4 +29,5 @@ public static class MatrixDiagonals
 
         return sum / cuont;
     }
+
 }
